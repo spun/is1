724c98a2630981e97a54d1409164329c9dab8dec
spun <neoappengine@gmail.com>
2012-03-10 22:16:29 +0100

    -Nuevos apartados en la pagina de juego. -La zona de dibujo ya recoge los datos que se pintan. -La zona de chat mantiene el scroll de la conversacion al final. -Nuevo estilo de la barra de scroll para webkit.

--- juego.py
+++ juego.py
@@ -21,5 +21,48 @@ class GamePage(webapp2.RequestHandler):
 
 
 
+#~ class GameBroadcast(webapp.RequestHandler):
+  #~ """This page is responsible for showing the game UI. It may also
+  #~ create a new game or add the currently-logged in uesr to a game."""
+#~ 
+	#~ def get(self):
+#~ 
+		#~ self.sess = session.Session('enginesession')
+		#~ if self.sess.load():
+			#~ user = UserDB().getUserByKey(self.sess.user)
+			#~ template_values['user'] = user
+#~ 
+		#~ if not user:
+			#~ self.redirect('/')
+			#~ return
+#~ 
+		#~ game_key = self.request.get('gamekey')
+		#~ game = None
+		#~ game = Game.get_by_key_name(game_key)
+		#~ if not game.userO and game.userX != user:
+        #~ # If this game has no 'O' player, then make the current user
+        #~ # the 'O' player.
+        #~ game.userO = user
+        #~ game.put()
+#~ 
+    #~ token = channel.create_channel(user.user_id() + game_key)
+    #~ template_values = {'token': token,
+                       #~ 'me': user.user_id(),
+                       #~ 'game_key': game_key
+                       #~ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app = webapp2.WSGIApplication([('/juego', GamePage)],
                               debug=True)
