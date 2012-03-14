1055c6ff060c2e6d74eadb239ee6aac784bb4e85
spun <neoappengine@gmail.com>
2012-03-15 00:17:07 +0100

    -Uso de chnApi en chat y canvas.

--- juego.py
+++ juego.py
@@ -1,12 +1,15 @@
 import os
 import webapp2
+import json
+import cgi
 
 from google.appengine.ext.webapp import template
 from google.appengine.api import channel
 
 import session
 from BD.clases import UserDB
-from BD.clases import Juego
+from BD.clases import Game
+from BD.clases import UsersInGame
 
 class GamePage(webapp2.RequestHandler):
 	def get(self):
@@ -19,25 +22,47 @@ class GamePage(webapp2.RequestHandler):
 			user = UserDB().getUserByKey(self.sess.user)
 			template_values['user'] = user
 			
-		path = os.path.join(os.path.dirname(__file__), 'juego.html')
-		self.response.out.write(template.render(path, template_values))
+		#path = os.path.join(os.path.dirname(__file__), 'juego.html')
+		#self.response.out.write(template.render(path, template_values))
+
+		game_key = self.request.get('gamekey')
+
+		# Si hay usuario identificado
+		if user:
+			# Si tenemos una gamekey
+			if game_key:
+				game = Game.get(game_key)
+
+			# Si existe un juego con esa gamekey
+			if game:				
+				users_by_game = UsersInGame.all()
+				users_by_game.filter("game =", game)
+				usersPlaying = users_by_game.fetch(30)
+				
+				users_by_game.filter("user =", user)				
+				results = users_by_game.get()
+
+				# Si el usuario identificado esta asignado al juego
+				if results:
+					token = channel.create_channel(str(results.key()))
+					template_values = {'token': token,
+						'user': user,
+						'game_key': game_key,
+						'users_playing': usersPlaying
+					}
+
+					path = os.path.join(os.path.dirname(__file__), 'juego.html')
+					self.response.out.write(template.render(path, template_values))
+					
+				else:
+					self.response.out.write("El usuario no esta asignado a este juego")
+
+				
+
+			else:
+				self.response.out.write("No hay una id de juego asignada")
+				
 			
-		#~ game_key = self.request.get('gamekey')
-#~ 
-		#~ if user:
-			#~ if game_key:
-				#~ game = Juego.get_by_key_name(game_key)
-				#~ 
-			#~ else:
-				#~ game_key = str(user.key())
-				#~ game = Juego()
-				#~ game.put()
-												#~ 
-						#~ 
-			#~ if game:
-				#~ tokId = str(game.key())+str(user.key())
-				#~ self.response.out.write(tokId)	
-				#~ token = channel.create_channel(tokId)
 				
 				
 				#~ template_values = {'token': token,
@@ -53,59 +78,84 @@ class GamePage(webapp2.RequestHandler):
 				#~ self.response.out.write('No such game')		
 #~ 
 					#~ 
-		#~ else:
-			#~ self.redirect('/login')
+		else:
+			self.redirect('/login')
 			#~ return
 
 
 
 
-class GameBroadcast(webapp2.RequestHandler):
+
+class GameBroadcastChat(webapp2.RequestHandler):
 	"""This page is responsible for showing the game UI. It may also
 	create a new game or add the currently-logged in uesr to a game."""
+	
+	def post(self):
+		game_key = self.request.get('g')
+		if game_key:
+			game = Game.get(game_key)
+			
+			if game:				
+				user = None
+				self.sess = session.Session('enginesession')
+				if self.sess.load():
+					user = UserDB().getUserByKey(self.sess.user)
+				
+				users_by_game = UsersInGame.all()
+				users_by_game.filter("game =", game)
 
-	def get(self):
-		self.response.out.write('here')
-		#~ user = None
-		#~ 
-		#~ self.sess = session.Session('enginesession')
-		#~ 
-		#~ if self.sess.load():
-			#~ user = UserDB().getUserByKey(self.sess.user)
-			#~ template_values['user'] = user
-#~ 
-		#~ if not user:
-			#~ self.redirect('/')
-			#~ return
+				results = users_by_game.fetch(30)
 
-		#~ game_key = self.request.get('gamekey')
-		#~ game = None
-		#~ game = Game.get_by_key_name(game_key)
-		#~ if not game.userO and game.userX != user:
-        #~ # If this game has no 'O' player, then make the current user
-        #~ # the 'O' player.
-        #~ game.userO = user
-        #~ game.put()
-
-    #~ token = channel.create_channel(user.user_id() + game_key)
-    #~ template_values = {'token': token,
-                       #~ 'me': user.user_id(),
-                       #~ 'game_key': game_key
-                       #~ }
-
+				messageRaw = {
+				"type": "chat", 
+				"content": {
+					"messaje": cgi.escape(self.request.get('d')),
+					"user": user.nick							
+					}
+				}				
+				message = json.dumps(messageRaw)
+				
+				for r in results:		
+					channel.send_message(str(r.key()), message)
 
 
 
+class GameBroadcastDraw(webapp2.RequestHandler):
+	"""This page is responsible for showing the game UI. It may also
+	create a new game or add the currently-logged in uesr to a game."""
+	
+	def post(self):
+		game_key = self.request.get('g')
+		if game_key:
+			game = Game.get(game_key)
+			
+			if game:				
+				user = None
+				self.sess = session.Session('enginesession')
+				if self.sess.load():
+					user = UserDB().getUserByKey(self.sess.user)
+				
+				users_by_game = UsersInGame.all()
+				users_by_game.filter("game =", game)
+				users_by_game.filter("user !=", user.key())
 
+				results = users_by_game.fetch(30)
 
-
-
-
-
+				messageRaw = {
+				"type": "draw", 
+				"content": {
+					"coord": cgi.escape(self.request.get('d'))						
+					}
+				}				
+				message = json.dumps(messageRaw)
+				
+				for r in results:		
+					channel.send_message(str(r.key()), message)
 
 
 
 
 app = webapp2.WSGIApplication([('/juego', GamePage),
-								('/gamebroadcast', GameBroadcast)],
+								('/gamebroadcast/chat', GameBroadcastChat),
+								('/gamebroadcast/draw', GameBroadcastDraw)],
                               debug=True)
