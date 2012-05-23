b1e1d8104b1c74ec3fe66a594c581bc468a4ee1b
Sephi <clophiroth@gmail.com>
2012-05-24 01:10:23 +0200

    Creada la página de ayuda

---
+++ ayuda.py
@@ -0,0 +1,15 @@
+﻿import os
+import webapp2
+
+from google.appengine.ext.webapp import template
+from google.appengine.api import channel
+
+import session
+
+class Ayuda(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__), 'ayuda.html')
+        self.response.out.write(template.render(path, template_values))
+
+app = webapp2.WSGIApplication([('/ayuda', Ayuda)], debug=True)
